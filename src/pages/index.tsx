e38ddfa482fd4eee0dcc285f0022eea2c7752f79
João Buch <joao.buch@clevertech.biz>
2022-04-12 01:41:35 -0300

    feat: static site generate (ssg)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import styles from "../styles/home.module.scss";
 import { SubscribeButton } from "../components/SubscribeButton";
 import Head from "next/head";
@@ -34,7 +34,7 @@ export default function Home({ product }) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KnavhGxrOKwyHnoV75aOHKV");
 
   const product = {
@@ -48,5 +48,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
